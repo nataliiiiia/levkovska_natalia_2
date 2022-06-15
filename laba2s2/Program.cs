3fe3874dd38ea52dca73a84e6f9a53aa4e3d6f79
Natalia Levkovska <levkovskayanatalia2004@gmail.com>
2022-06-15 12:06:48 +0300

    add

---
+++ laba2s2/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba2s2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Text("x of vertex 1");
+            double ver1x = double.Parse(Console.ReadLine());
+            Text("y of vertex 1");
+            double ver1y = double.Parse(Console.ReadLine());
+            Text("x of vertex 2");
+            double ver2x = double.Parse(Console.ReadLine());
+            Text("y of vertex 2");
+            double ver2y = double.Parse(Console.ReadLine());
+            Text("x of vertex 3");
+            double ver3x = double.Parse(Console.ReadLine());
+            Text("y of vertex 3");
+            double ver3y = double.Parse(Console.ReadLine());
+            Console.Clear();
+            Triangle triangle = new(ver1x, ver1y, ver2x, ver2y, ver3x, ver3y);
+            WriteStats(triangle);
+            OpenJsonFile();
+            CreateJsonFile(triangle);
+        }
+
+        static void Text(string name)
+        {
+            Console.WriteLine($"Enter the {name}:");          
+        }
+            
+        static void CreateJsonFile(Triangle triangle)
+        {
+            using FileStream fs = new("triangle.json", FileMode.OpenOrCreate);
+            string jsonObject = JsonConvert.SerializeObject(triangle);
+            byte[] bytes = Encoding.Default.GetBytes(jsonObject);
+            fs.Write(bytes);
+        }
+        static void OpenJsonFile()
+        {
+            using FileStream fs = File.OpenRead("triangle.json");
+            byte[] bytes = new byte[fs.Length];
+            fs.Read(bytes, 0, bytes.Length);
+            string jsonObject = Encoding.Default.GetString(bytes);
+            Triangle newTriangle = JsonConvert.DeserializeObject<Triangle>(jsonObject);
+            Console.WriteLine("\nTriangle from Json file");
+            WriteStats(newTriangle);
+        }
+        static void WriteStats(Triangle triangle)
+        {
+            Console.WriteLine($"Vertex 1 ({triangle.Vertex1X}; {triangle.Vertex1Y})");
+            Console.WriteLine($"Vertex 2 ({triangle.Vertex2X}; {triangle.Vertex2Y})");
+            Console.WriteLine($"Vertex 3 ({triangle.Vertex3X}; {triangle.Vertex3Y})");
+            Console.WriteLine("The type of this triangle: " + triangle.Type());
+            if (triangle.Type() != "It's not a triangle.")
+            {
+                Console.WriteLine("Side 1: " + Math.Round(triangle.Side1(), 1));
+                Console.WriteLine("Side 2: " + Math.Round(triangle.Side2(), 1));
+                Console.WriteLine("Side 3: " + Math.Round(triangle.Side3(), 1));
+                Console.WriteLine("Perimeter: " + Math.Round(triangle.Perimeter(), 1));
+                Console.WriteLine("Square: " + Math.Round(triangle.Square(), 1));
+                Console.WriteLine($"Cross of medians ({Math.Round(triangle.MedianX(), 1)}; {Math.Round(triangle.MedianY(), 1)})");
+            }
+        }
+
+
+        
+    }
+}
---
+++ laba2s2/Triangle.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace laba2s2
+{
+    class Triangle
+    {
+        public double Vertex1X { get; set; }
+        public double Vertex1Y { get; set; }
+        public double Vertex2X { get; set; }
+        public double Vertex2Y { get; set; }
+        public double Vertex3X { get; set; }
+        public double Vertex3Y { get; set; }
+
+        public double Side1()
+        {
+            double side1 = Math.Sqrt(Math.Pow((Vertex2X - Vertex1X), 2) + Math.Pow((Vertex2Y - Vertex1Y), 2));
+            return side1;
+        }
+        public double Side2()
+        {
+            double side2 = Math.Sqrt(Math.Pow((Vertex3X - Vertex2X), 2) + Math.Pow((Vertex3Y - Vertex2Y), 2));
+            return side2;
+        }
+        public double Side3()
+        {
+            double side3 = Math.Sqrt(Math.Pow((Vertex3X - Vertex1X), 2) + Math.Pow((Vertex3Y - Vertex1Y), 2));
+            return side3;
+        }
+        public double Perimeter()
+        {
+            double side1 = Side1();
+            double side2 = Side2();
+            double side3 = Side3();
+            double perimeter = side1 + side2 + side3;
+            return perimeter;
+        }
+        public double Square()
+        {
+            double halfp = Perimeter() / 2;
+            double square = Math.Sqrt(halfp * (halfp - Side1()) * (halfp - Side2()) * (halfp - Side3()));
+            return square;
+        }
+        public double MedianX()
+        {
+            double Xmed = (Vertex1X + Vertex2X) / 2;
+            double Xmid = (Vertex3X + 2 * Xmed) / 3;
+            return Xmid;
+        }
+        public double MedianY()
+        {
+            double Ymed = (Vertex1Y + Vertex2Y) / 2;
+            double Ymid = (Vertex3Y + 2 * Ymed) / 3;
+            return Ymid;
+        }
+        public string Type()
+        {
+            if (Math.Round(Square(),0) != 0 == true)
+            {
+                if (Side1() + Side2() > Side3() & Side1() + Side3() > Side2() & Side2() + Side3() > Side1())
+                {
+                    if (Side1() > Side2() & Side1() > Side3())
+
+                    {
+                        if (((Math.Pow(Side2(), 2) + Math.Pow(Side3(), 2) - Math.Pow(Side1(), 2)) / (2 * Side2() * Side3())) > -0.1 & ((Math.Pow(Side2(), 2) + Math.Pow(Side3(), 2) - Math.Pow(Side1(), 2)) / (2 * Side2() * Side3())) < 0.1)
+                        {
+                            return "Right triangle";
+                        }
+                        else if (((Math.Pow(Side2(), 2) + Math.Pow(Side3(), 2) - Math.Pow(Side1(), 2)) / (2 * Side2() * Side3())) < 0)
+                        {
+                            return "Obtuse triangle";
+                        }
+                        else if (((Math.Pow(Side2(), 2) + Math.Pow(Side3(), 2) - Math.Pow(Side1(), 2)) / (2 * Side2() * Side3())) > 0)
+                        {
+                            return "Acute triangle";
+                        }
+
+                    }
+                    else if (Side2() > Side1() & Side2() > Side3())
+                    {
+                        if (((Math.Pow(Side1(), 2) + Math.Pow(Side3(), 2) - Math.Pow(Side2(), 2)) / (2 * Side1() * Side3())) > -0.1 & (Math.Pow(Side1(), 2) + Math.Pow(Side3(), 2) - Math.Pow(Side2(), 2)) / (2 * Side1() * Side3()) < 0.1)
+                        {
+                            return "Right triangle";
+                        }
+                        else if (((Math.Pow(Side1(), 2) + Math.Pow(Side3(), 2) - Math.Pow(Side2(), 2)) / (2 * Side1() * Side3())) < 0)
+                        {
+                            return "Obtuse triangle";
+                        }
+                        else if (((Math.Pow(Side1(), 2) + Math.Pow(Side3(), 2) - Math.Pow(Side2(), 2)) / (2 * Side1() * Side3())) > 0)
+                        {
+                            return "Acute triangle";
+                        }
+
+                    }
+                    else if (Side3() > Side1() & Side3() > Side2())
+                    {
+                        if (((Math.Pow(Side1(), 2) + Math.Pow(Side2(), 2) - Math.Pow(Side3(), 2)) / (2 * Side1() * Side2())) > -0.1 & ((Math.Pow(Side1(), 2) + Math.Pow(Side2(), 2) - Math.Pow(Side3(), 2)) / (2 * Side1() * Side2())) < 0.1)
+                        {
+                            return "Right triangle";
+                        }
+                        else if (((Math.Pow(Side1(), 2) + Math.Pow(Side2(), 2) - Math.Pow(Side3(), 2)) / (2 * Side1() * Side2())) < 0)
+                        {
+                            return "Obtuse triangle";
+                        }
+                        else if (((Math.Pow(Side1(), 2) + Math.Pow(Side2(), 2) - Math.Pow(Side3(), 2)) / (2 * Side1() * Side2())) > 0)
+                        {
+                            return "Acute triangle";
+                        }
+
+                    }
+                }
+                return "It's not a triangle.";
+            }
+            else
+            return "It's not a triangle.";
+        }
+        public Triangle(double Ver1x, double Ver1y, double Ver2x, double Ver2y, double Ver3x, double Ver3y)
+        {
+            Vertex1X = Ver1x;
+            Vertex1Y = Ver1y;
+            Vertex2X = Ver2x;
+            Vertex2Y = Ver2y;
+            Vertex3X = Ver3x;
+            Vertex3Y = Ver3y;
+        }
+
+
+    }
+}
